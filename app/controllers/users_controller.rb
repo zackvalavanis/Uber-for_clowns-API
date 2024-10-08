204b2c5b780023c703cca7dd860cba3f5ae4e18c
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-07 20:52:38 -0500

    added users create action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email], 
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+      )
+      if @user.save 
+        render :show
+      else
+        render json: {error: @user.errors.full_messages}, status: :bad_request
+      end
+  end 
+end
