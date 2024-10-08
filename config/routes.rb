04c5462d03b4b371e6e493f55fbf87fc5b1c8ac8
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-07 20:59:12 -0500

    added the jwt action

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "users/:id" => "users#show"
   post "users" => "users#create"
+
+  get "sessions/:id" => "sessions#show"
+  post "sessions" => "sessions#create"
 
   # Defines the root path route ("/")
   # root "posts#index"
