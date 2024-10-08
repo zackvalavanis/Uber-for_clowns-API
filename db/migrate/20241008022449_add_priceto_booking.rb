2d0fa5225943f7bbbc12da9ee6514c1efcdc9c90
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-08 18:12:40 -0500

    updates

--- db/migrate/20241008022449_add_priceto_booking.rb
+++ db/migrate/20241008022449_add_priceto_booking.rb
@@ -1,4 +1,5 @@
 class AddPricetoBooking < ActiveRecord::Migration[7.1]
   def change
+    add_column :bookings, :price, :integer 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_08_022104) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_08_022449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_08_022104) do
     t.integer "clown_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
   end
 
   create_table "clowns", force: :cascade do |t|
